83e07b71cc0935b7ee513dfbcc65a48c40317dbd
halo <dongjin778@naver.com>
2025-11-06 14:36:37 +0900

    feat : 비밀번호 변경 기능 구현

--- public/pages/user/password/password.js
+++ public/pages/user/password/password.js
@@ -2,6 +2,8 @@ import { renderNavigator } from "/component/common/header/navigator/navigator.js
 import { renderPasswordWrap } from "../../../component/user/password/password_wrap/password_wrap.js";
 import { renderFooter } from "/component/common/footer/footer_inner.js";
 
+import {addPasswordChangeEvent} from "/service/user/password/addPasswordChangeEvent.js"
+
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("header")
   const password_wrap = document.getElementById("password_wrap");
@@ -10,5 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
   renderNavigator(header);
   renderPasswordWrap(password_wrap);
   renderFooter(footer);
+
+  // 1. 비밀번호 변경 이벤트 적용
+  addPasswordChangeEvent(password_wrap);
 
 });
