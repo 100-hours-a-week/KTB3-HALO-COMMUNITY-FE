b8549cda5c39cc69902495a34a6ce29904c9e911
halo <dongjin778@naver.com>
2025-11-22 20:31:23 +0900

    feat : https 로 변경

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://3.39.73.203:8080/api/v1";
-export const Lcoal_API_BASE = "http://localhost:8080/api/v1";
+export const API_BASE = "https://notmean.duckdns.org/api/v1";
+export const API_BASE_LOCAL = "http://localhost:8080/api/v1";
