3c70030815b1fa1880f0cd5b6ad2a550e7acc813
halo <dongjin778@naver.com>
2025-11-29 00:18:36 +0900

    feat: 배포 파일 수정

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://Our-Planet-983823842.ap-northeast-2.elb.amazonaws.com/api/v1";
+export const API_BASE = "http://our-planet-983823842.ap-northeast-2.elb.amazonaws.com/:3000";
 export const API_BASE_LOCAL = "http://localhost:8080/api/v1";
