5673a21b740d303495541624d2bc3137056f47df
halo <dongjin778@naver.com>
2025-11-29 20:07:21 +0900

    feat : https 적용

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://www.ouruniverse.cloud/api/v1";
+export const API_BASE = "https://www.ouruniverse.cloud/api/v1";
 export const API_BASE_LOCAL = "http://localhost:8080/api/v1";
