98cd913b89ce97e6bb147c386eb4503074fea202
halo <dongjin778@naver.com>
2025-12-04 23:28:11 +0900

    style : 이미지 사이즈 문제로 인한 카테부 베너 해제

--- public/component/auth/gladbanner/gladbanner.js
+++ public/component/auth/gladbanner/gladbanner.js
@@ -2,8 +2,7 @@ export function renderGladBanner(rootEl) {
     // 광고 배너 이미지 배열
     const bannerImages = [
         'https://not-me-be.s3.ap-northeast-2.amazonaws.com/Unsaved/2025/11/13/image+(1).avif',
-        'https://cdn.srtimes.kr/news/photo/201911/50047_44451_2910.jpg',
-        'https://not-me-be.s3.ap-northeast-2.amazonaws.com/Unsaved/2025/11/13/header_efc7a909-453c-4eb8-968c-69919e248b2b.jpg'
+        'https://cdn.srtimes.kr/news/photo/201911/50047_44451_2910.jpg'
     ];
     
     // 랜덤으로 이미지 선택
